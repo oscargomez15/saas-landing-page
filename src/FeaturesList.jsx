5edb5bd0920c84e42cf56482cb9ecccd1a90577c
Oscar G <murdexchannel@gmail.com>
2024-02-03 21:33:15 -0500

    deploy to ghpages

--- src/FeaturesList.jsx
+++ src/FeaturesList.jsx
@@ -4,12 +4,12 @@ import { faHandSparkles, faScaleBalanced, faUsersLine, faWandMagicSparkles } fro
 
 export const Features = () => {
   return (
-    <section className='flex flex-col lg:w-6/12 w-10/12 mx-auto min-h-dvh gap-36 justify-center items-center mt-64'>
+    <section className='flex flex-col lg:w-6/12 w-10/12 mx-auto min-h-dvh gap-36 justify-center items-center pt-64'>
         <h2 className='md:text-7xl text-4xl text-white font-poppins font-thin text-center'>Revolutionize how you make decisions, anticipate trends, and <span className='font-bold'>optimize operations</span></h2>
-        <div className='features flex gap-16 flex-wrap justify-around'>
-            <article className='bg-[#374357] md:basis-1/3 rounded-3xl flex items-center justify-center shadow-2xl h-48'>
+        <div className='features flex gap-16 flex-wrap justify-center items-center'>
+            <article className='bg-[#374357] md:basis-2/3 rounded-3xl flex items-center justify-center shadow-2xl h-48'>
                 {/* <Lottie animationData={scale} className='w-[300px]'></Lottie> */}
-                <div className='flex items-center justify-center'>
+                <div className='basis-1/3 flex items-center justify-center'>
                     <FontAwesomeIcon icon={faScaleBalanced} className='md:h-16 h-8 text-white bg-lime-400 rounded-md p-4'/>
                 </div>
                 <div className='flex flex-col justify-between basis-full'>
@@ -21,7 +21,7 @@ export const Features = () => {
                 </div>
             </article>
 
-            <article className='bg-[#374357] md:basis-1/3 rounded-3xl flex items-center justify-center shadow-2xl h-48'>
+            <article className='bg-[#374357] md:basis-2/3 rounded-3xl flex items-center justify-center shadow-2xl h-48'>
                 {/* <Lottie animationData={graph} className='w-[300px]'></Lottie> */}
                 <div className='basis-1/3 flex items-center justify-center'>
                     <FontAwesomeIcon icon={faWandMagicSparkles} className='md:h-16 h-8 text-white bg-cyan-400 rounded-md p-4'/>
@@ -34,7 +34,7 @@ export const Features = () => {
                  </div>
             </article>
 
-            <article className='bg-[#374357] md:basis-1/3 rounded-3xl flex items-center justify-center shadow-2xl h-48'>
+            <article className='bg-[#374357] md:basis-2/3 rounded-3xl flex items-center justify-center shadow-2xl h-48'>
                 {/* <Lottie animationData={data} className='w-[300px]'></Lottie> */}
                 <div className='basis-1/3 flex items-center justify-center'>
                     <FontAwesomeIcon icon={faUsersLine} className='md:h-16 h-8 text-white bg-yellow-400 rounded-md p-4'/>
@@ -46,7 +46,7 @@ export const Features = () => {
                  </div>
             </article>
 
-            <article className='bg-[#374357] md:basis-1/3 rounded-3xl flex items-center justify-center shadow-2xl h-48'>
+            <article className='bg-[#374357] md:basis-2/3 rounded-3xl flex items-center justify-center shadow-2xl h-48'>
                 {/* <Lottie animationData={data} className='w-[300px]'></Lottie> */}
                 <div className='basis-1/3 flex items-center justify-center'>
                     <FontAwesomeIcon icon={faHandSparkles} className='md:h-16 h-8 text-white bg-orange-400 rounded-md p-4'/>
